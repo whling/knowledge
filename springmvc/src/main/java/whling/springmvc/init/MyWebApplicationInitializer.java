6c6a6634e376ffdca4552521afbfeaef10a67384
whling <1769778747@qq.com>
2020-03-19 00:14:22 +0800

    添加注释

--- springmvc/src/main/java/whling/springmvc/init/MyWebApplicationInitializer.java
+++ springmvc/src/main/java/whling/springmvc/init/MyWebApplicationInitializer.java
@@ -9,6 +9,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+/**
+ * 在servlet3.0之后有一个规范就是servlet容器在启动之后会通过SPI机制，
+ * 解析配置在classpath路径下META-INF/services/javax.servlet.ServletContainerInitializer配置的类
+ * 里面的类需要实现ServletContainerInitializer接口。
+ *
+ * 该类可以配置注解@HandlesTypes() 来关注自己想要关注的类
+ * 参考 {@link org.springframework.web.SpringServletContainerInitializer}
+ */
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
