2e82a011877715473e9dd6b20a5f8b96f7b094ea
whling <1769778747@qq.com>
2020-03-18 10:45:45 +0800

    add default val

--- springmvc/src/main/java/whling/springmvc/controller/DemoController.java
+++ springmvc/src/main/java/whling/springmvc/controller/DemoController.java
@@ -4,13 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Objects;
+
 @Controller
 public class DemoController {
 
     @RequestMapping(value = "/demo")
     @ResponseBody
     public String demo(String name) {
+
         System.out.println("demo controller ..");
+
+        if (Objects.isNull(name)) {
+            return "hello world";
+        }
+
         return name;
     }
 }
