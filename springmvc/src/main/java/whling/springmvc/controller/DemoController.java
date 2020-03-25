05ccdab5d814c1694340ff6c2389d1b0d3e1dcaa
whling <1769778747@qq.com>
2020-03-26 00:57:42 +0800

    netty and lombok test

--- springmvc/src/main/java/whling/springmvc/controller/DemoController.java
+++ springmvc/src/main/java/whling/springmvc/controller/DemoController.java
@@ -1,9 +1,12 @@
 package whling.springmvc.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -11,10 +14,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 对同一资源进行并发修改，会有线程安全问题
- *  解决思路主要有：
- *      1。排队（加锁，同一时刻只保持某一个线程能够访问）
- *      2。投票（paxos/raft协议之类，同一时刻也只能有一个主导者）
- *      3。避免（GIT分布式版本控制，做多版本，冲突解决算法/ThreadLocal做多线程隔离，对结果的获取其实就是一种很简单的结果合并，冲突解决）
+ * 解决思路主要有：
+ * 1。排队（加锁，同一时刻只保持某一个线程能够访问）
+ * 2。投票（paxos/raft协议之类，同一时刻也只能有一个主导者）
+ * 3。避免（GIT分布式版本控制，做多版本，冲突解决算法/ThreadLocal做多线程隔离，对结果的获取其实就是一种很简单的结果合并，冲突解决）
  */
 @Controller
 public class DemoController {
@@ -35,18 +38,28 @@ public class DemoController {
 
     @RequestMapping(value = "/add")
     @ResponseBody
-    public String add(String name) {
+    public void add(String name, HttpServletRequest request,
+                    HttpServletResponse response) throws Exception {
 
         count++;
         Val val = tl.get();
         val.setCount(val.getCount() + 1);
-        System.out.println("demo controller .." + count);
-
-        if (Objects.isNull(name)) {
-            return "hello world " + count;
+        if (StringUtils.isNotEmpty(String.valueOf(count))) {
+            System.out.println("demo controller .." + count);
         }
 
-        return name;
+        if (Objects.isNull(name)) {
+            request.setAttribute("attr", "hello world " + count);
+        }
+
+//        request.getRequestDispatcher("/dispatch").forward(request, response);
+        response.sendRedirect("/dispatch?attr=hello");
+    }
+
+    @RequestMapping(value = "/dispatch")
+    @ResponseBody
+    public String dispatch(HttpServletRequest request) {
+        return request.getAttribute("attr") != null ? (String) request.getAttribute("attr") : "hello world";
     }
 
     @RequestMapping(value = "/stat")
